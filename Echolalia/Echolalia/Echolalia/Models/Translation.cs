8be1012fb66d77489fe5b1b9067c8bfe547eb27b
Mark <smdmrr00@gmail.com>
2023-03-19 13:15:26 +0100

    add DB and model of DB

---
+++ Echolalia/Echolalia/Echolalia/Models/Translation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace Echolalia.Models
+{
+	public class Translation
+	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		public string TypeOfItem { get; set; }
+		public string Item { get; set; }
+		public string TranslateItem { get; set; }
+
+	}
+}
+
